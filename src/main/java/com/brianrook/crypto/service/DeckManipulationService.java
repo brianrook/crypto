dba2b2b66bd99650ed986e34108de124b6919740
brian <brian.rook@gmail.com>
2020-08-02 12:23:45 -0600

    first manipulation and tests

--- src/main/java/com/brianrook/crypto/service/DeckManipulationService.java
+++ src/main/java/com/brianrook/crypto/service/DeckManipulationService.java
@@ -12,4 +12,19 @@ public class DeckManipulationService {
          return cardStr;
      }
 
+     public static Deck firstManipulation(Deck deckIn){
+         int jokerPosition = deckIn.getCards().lastIndexOf(Deck.JOKER_A);
+
+         //if the joker is not at the end of the deck, move down one
+         if (jokerPosition!=Deck.DECK_SIZE-1){
+             deckIn.getCards().remove(jokerPosition);
+             deckIn.getCards().add(jokerPosition+1, Deck.JOKER_A);
+         }
+         //joker in last position, move between front 2 cards
+         else{
+             deckIn.getCards().remove(jokerPosition);
+             deckIn.getCards().add(1, Deck.JOKER_A);
+         }
+         return deckIn;
+     }
 }
--- src/test/java/com/brianrook/crypto/service/DeckManipulationServiceTest.java
+++ src/test/java/com/brianrook/crypto/service/DeckManipulationServiceTest.java
@@ -14,4 +14,27 @@ public class DeckManipulationServiceTest {
         assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 jokerA jokerB",
                 DeckManipulationService.writeDeck(myDeck));
     }
+
+    @Test
+    public void testFirstManipulationJokerANotAtEnd(){
+        Deck myDeck = new Deck();
+
+        Deck returnDeck = DeckManipulationService.firstManipulation(myDeck);
+
+        //joker should be in last position
+        assertEquals(Deck.JOKER_A, returnDeck.getCards().get(Deck.DECK_SIZE-1));
+    }
+
+    @Test
+    public void testFirstManipulationJokerAAtEnd(){
+        //given
+        Deck myDeck = new Deck();
+
+        //when
+        Deck returnDeck = DeckManipulationService.firstManipulation(myDeck);
+        returnDeck = DeckManipulationService.firstManipulation(myDeck);
+
+        //joker should be in second position
+        assertEquals(Deck.JOKER_A, returnDeck.getCards().get(1));
+    }
 }
